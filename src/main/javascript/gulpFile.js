ac09ce930499ec600fcad2929111cb02b655c11f
Christopher Kolstad <christopher.kolstad@gmail.com>
2014-10-27 19:30:13 +0100

    Removed advanced optimization, premature

--- src/main/javascript/gulpFile.js
+++ src/main/javascript/gulpFile.js
@@ -3,28 +3,14 @@ var gulp = require('gulp'),
     watch = require('gulp-watch'),
     notify = require('gulp-notify');
     closureCompiler = require('gulp-closure-compiler'),
-    bower = require('gulp-bower'),
     destDir = '../webapp/WEB-INF/js';
 
-gulp.task('bower', function () {
-    return bower();
-});
 gulp.task('default', function () {
     gulp.src('jsx/*.jsx')
         .pipe(react())
         .pipe(closureCompiler({
             compilerPath: 'bower_components/closure-compiler/compiler.jar',
             fileName: 'build.js',
-            compilerFlags: {
-                closure_entry_point: 'app.main',
-                compilation_level: 'ADVANCED_OPTIMIZATIONS',
-                define: [
-                    "goog.DEBUG=false"
-                ],
-                externs: [
-                    'bower_components'
-                ]
-            }
         }))
         .pipe(gulp.dest(destDir))
         .pipe(notify('JSX files have been optimizaed and put into ' +destDir))
